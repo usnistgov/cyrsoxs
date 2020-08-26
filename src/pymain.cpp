6a01c597cf10ef5496766a9916c87a46158698ab
maksbh <kumar.saurabh.mnc14@gmail.com>
2020-08-26 00:10:08 -0500

    Broken pipeline. Debugging

--- src/pymain.cpp
+++ src/pymain.cpp
@@ -4,10 +4,14 @@
 
 
 #include <pybind11/pybind11.h>
-#include <Input/Input.h>
+#include <Input/InputData.h>
+#include <Output/writeH5.h>
 
+namespace py = pybind11;
 
 PYBIND11_MODULE(Cy_RSoXS,module)
 {
-
+    module.doc() = "pybind11  plugin for Cy-RSoXS";
+    py::class_<InputData>(module,"InputData")
+            .def("Dimension",InputData::numX);
 }
