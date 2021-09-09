12d8c34e54bdb78f3865a27988d4f8baec9f5b95
maksbh <kumar.saurabh.mnc14@gmail.com>
2021-09-08 21:44:10 -0500

    Copyright updated

--- include/Output/writeVTI.h
+++ include/Output/writeVTI.h
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 // MIT License
 //
-//Copyright (c) 2019 -2020 Iowa State University
+//Copyright (c) 2019 -2021 Iowa State University
 //
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
