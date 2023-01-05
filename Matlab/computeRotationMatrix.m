9e09b0e85adf4f421931794e736166ec4fa44da4
Peter Dudenas <peter.dudenas@nist.gov>
2023-01-04 16:15:35 -0800

    remove deprecated matlab scripts

--- Matlab/computeRotationMatrix.m
+++
@@ -1,9 +0,0 @@
-function UU = computeRotationMatrix(A,B)
-G =  [ dot(A,B) -norm(cross(A,B)) 0;...
-        norm(cross(A,B)) dot(A,B)  0;...
-        0              0           1];
-
-F = [ A (B-dot(A,B)*A)/norm(B-dot(A,B)*A) cross(B,A) ];
-
-UU =  F*G*inv(F);
-end
--- Matlab/scatter3DRot.m
+++
@@ -1,6 +0,0 @@
-function [val] = scatter3DRot(vec1,vec2,k)
-
-vec = (k*k*eye(3) - (vec1*vec1'))*vec2;
-val = norm(vec(1))^2 + norm(vec(2))^2 + norm(vec(3))^2;
-end
-
